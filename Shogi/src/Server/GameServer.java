9c84bbbf451285469ee8967aec7dcd26f31fa7bd
nbenson-5831 <nbenson@cub.uca.edu>
2025-04-16 21:55:13 -0500

    updating GameServer handleMessage from client

--- Shogi/src/Server/GameServer.java
+++ Shogi/src/Server/GameServer.java
@@ -1,14 +1,20 @@
 package Server;
 
 import java.awt.*;
+
 import javax.swing.*;
 
 import Database.GameDatabase;
 import UserManagement.CreateAccountData;
 import UserManagement.LoginData;
+import LobbyManagement.BrowseLobbyData;
+import LobbyManagement.LobbyData;
 
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import ocsf.server.AbstractServer;
 import ocsf.server.ConnectionToClient;
 
@@ -19,6 +25,8 @@ public class GameServer extends AbstractServer
   private JLabel status;
   private boolean running = false;
   private GameDatabase database = new GameDatabase();
+  private List<LobbyData> currentLobbies = new ArrayList<>();
+  private int lobbyId;
 
   // Constructor for initializing the server with default settings.
   public GameServer()
@@ -32,7 +40,7 @@ public class GameServer extends AbstractServer
   {
     return running;
   }
-  
+
   public void setDatabase(GameDatabase database)
   {
     this.database = database;
@@ -57,7 +65,7 @@ public class GameServer extends AbstractServer
         result = new Error("The username and password are incorrect.");
         //log.append("Client " + arg1.getId() + " failed to log in\n");
       }
-      
+
       // Send the result to the client.
       try
       {
@@ -68,7 +76,7 @@ public class GameServer extends AbstractServer
         return;
       }
     }
-    
+
     // If we received CreateAccountData, create a new account.
     else if (arg0 instanceof CreateAccountData)
     {
@@ -76,7 +84,7 @@ public class GameServer extends AbstractServer
       CreateAccountData data = (CreateAccountData)arg0;
       Object result;
       if (!database.usernameExists(data.getUsername()) &&
-  		    database.createUser(data.getUsername(), data.getPassword()))
+              database.createUser(data.getUsername(), data.getPassword()))
       {
         result = "CreateAccountSuccessful";
         //log.append("Client " + arg1.getId() + " created a new account called " + data.getUsername() + "\n");
@@ -86,7 +94,7 @@ public class GameServer extends AbstractServer
         result = new Error("This username is already in use");
         //log.append("Client " + arg1.getId() + " failed to create a new account\n");
       }
-      
+
       // Send the result to the client.
       try
       {
@@ -97,10 +105,64 @@ public class GameServer extends AbstractServer
         return;
       }
     }
+    else if(arg0 instanceof String msg)
+    {
+      if(msg.equals("CREATE_LOBBY"))
+      {
+        User owner = (User) arg1.getInfo("user");
+        int newlobbyId = lobbyId++;
+        LobbyData newLobby = new LobbyData(newlobbyId, owner, 120, true);
+        currentLobbies.add(newLobby);
+
+        try {
+          arg1.sendToClient(newLobby);
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        //sendBrowseLobbyUpdate();
+
+      }
+    }
+    else if(arg0 instanceof String msg)
+    {
+      if(msg.equals("JOIN_LOBBY"))
+      {
+        User joiningUser = (User) arg1.getInfo("user");
+        for (LobbyData lobby : currentLobbies) {
+          if (lobby.getLobbyId() == lobbyId) {
+            if (lobby.getOpponent() == null) {
+              lobby.setOpponent(joiningUser);
+              try {
+                arg1.sendToClient(lobby);
+              } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+              }
+            } else {
+              try {
+                arg1.sendToClient(new Error("BrowseLobby Lobby is already full."));
+              } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+              }
+            }
+            return;
+          }
+        }
+
+        try {
+          arg1.sendToClient(new Error("BrowseLobby Lobby not found."));
+        } catch (IOException e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+      }
+    }
   }
 
   // Method that handles listening exceptions by displaying exception information.
-  public void listeningException(Throwable exception) 
+  public void listeningException(Throwable exception)
   {
     running = false;
     //status.setText("Exception occurred while listening");
@@ -109,3 +171,4 @@ public class GameServer extends AbstractServer
     //log.append("Press Listen to restart server\n");
   }
 }
+
